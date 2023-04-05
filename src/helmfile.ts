5a1df669f5081b50c03f28be88098704a7489034
Dmytro Bondar <git@bonddim.com>
2023-04-05 14:01:09 +0200

    Fix prettier issues

--- src/helmfile.ts
+++ src/helmfile.ts
@@ -1,4 +1,4 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
 import {
   arch,
   cacheDir,
@@ -7,25 +7,25 @@ import {
   platform,
   resloveCached,
   resolveLatest
-} from './helpers'
+} from './helpers';
 
 export async function installHelmfile(version: string): Promise<void> {
   if (version === 'latest') {
-    version = await resolveLatest('helmfile', 'helmfile')
+    version = await resolveLatest('helmfile', 'helmfile');
   }
 
-  let toolPath = await resloveCached('helmfile', version)
+  let toolPath = await resloveCached('helmfile', version);
   if (toolPath) {
-    core.info(`Found in cache @ ${toolPath}`)
-    core.addPath(toolPath)
-    return
+    core.info(`Found in cache @ ${toolPath}`);
+    core.addPath(toolPath);
+    return;
   }
-  core.info(`Attempting to download helmfile ${version}...`)
-  const baseUrl = 'https://github.com/helmfile/helmfile/releases/download'
-  const binVersion = version.replace(/^v/, '')
-  const downloadUrl = `${baseUrl}/${version}/helmfile_${binVersion}_${platform}_${arch}.tar.gz`
-  const downloadPath = await download(downloadUrl)
-  const extractedPath = await extract(downloadPath)
-  toolPath = await cacheDir(extractedPath, 'helmfile', version)
-  core.addPath(toolPath)
+  core.info(`Attempting to download helmfile ${version}...`);
+  const baseUrl = 'https://github.com/helmfile/helmfile/releases/download';
+  const binVersion = version.replace(/^v/, '');
+  const downloadUrl = `${baseUrl}/${version}/helmfile_${binVersion}_${platform}_${arch}.tar.gz`;
+  const downloadPath = await download(downloadUrl);
+  const extractedPath = await extract(downloadPath);
+  toolPath = await cacheDir(extractedPath, 'helmfile', version);
+  core.addPath(toolPath);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
-import * as core from '@actions/core'
-import * as exec from '@actions/exec'
-import {installHelm, installHelmPlugins} from './helm'
-import {installHelmfile} from './helmfile'
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import {installHelm, installHelmPlugins} from './helm';
+import {installHelmfile} from './helmfile';
 
 async function run(): Promise<void> {
   try {
-    const helmfileArgs = core.getInput('helmfile-args')
-    const helmfileVersion = core.getInput('helmfile-version')
-    const helmVersion = core.getInput('helm-version')
-    const helmPlugins = core.getInput('helm-plugins')
+    const helmfileArgs = core.getInput('helmfile-args');
+    const helmfileVersion = core.getInput('helmfile-version');
+    const helmVersion = core.getInput('helm-version');
+    const helmPlugins = core.getInput('helm-plugins');
 
-    core.debug(`helmfile-args: ${helmfileArgs}`)
-    core.debug(`helmfile-version: ${helmfileVersion}`)
-    core.debug(`helm-version: ${helmVersion}`)
-    core.debug(`helm-plugins: ${helmPlugins}`)
+    core.debug(`helmfile-args: ${helmfileArgs}`);
+    core.debug(`helmfile-version: ${helmfileVersion}`);
+    core.debug(`helm-version: ${helmVersion}`);
+    core.debug(`helm-plugins: ${helmPlugins}`);
 
     await Promise.all([
       installHelm(helmVersion),
       installHelmfile(helmfileVersion)
-    ])
+    ]);
 
     if (helmPlugins.length > 0) {
-      await installHelmPlugins(helmPlugins.split(','))
+      await installHelmPlugins(helmPlugins.split(','));
     }
 
-    await exec.exec(`helmfile ${helmfileArgs}`)
+    await exec.exec(`helmfile ${helmfileArgs}`);
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
