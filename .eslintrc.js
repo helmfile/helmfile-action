2cbaf31dd5d671883f528d869f944ad51b38cb46
yxxhero <aiopsclub@163.com>
2024-12-22 09:00:01 +0800

    fix lint issue
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 // This is a reusable configuration file copied from https://github.com/actions/reusable-workflows/tree/main/reusable-configurations. Please don't make changes to this file as it's the subject of an automatic update.
 module.exports = {
-  ignorePatterns: ["dist/*", "lib/*", "node_modules/*", "jest.config.js"],
+  ignores: ["dist/**", "lib/**", "node_modules/**", "jest.config.js"],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
